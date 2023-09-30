3fa79bd2a390b7f78c0d6e958ddc0f3488760872
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-09-30 20:02:24 +0900

    틀림

---
+++ 2017/3회/3번.c
@@ -0,0 +1,16 @@
+//다음 C언어로 구현된 프로그램을 분석하여 그 실행 결과를 쓰시오.
+#include <stdio.h>
+int power(int data, int exp) {
+  int i, result = 1;
+  for (i = 0; i < exp; i++) {
+      result = result * data;
+  }
+  return result;
+}
+int main() {
+  printf("%d\n", power(2, 10));
+  return 0;
+}
+
+//답
+//1024
