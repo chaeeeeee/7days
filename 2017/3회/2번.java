5c723078af815b38adfe7a693cbd41ce32ab37d1
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-09-30 19:58:01 +0900

    맞춤

---
+++ 2017/3회/2번.java
@@ -0,0 +1,21 @@
+//다음은 배열에 저장된 5개의 자료 중 가장 큰 값과 가장 작은 값을 찾아 출력하는 프로그램을 Java 언어로 구현한 것이다. 프로그램을 분석하여 괄호에 해당하는 답안을 <답란>에 쓰시오.
+public class Main {
+  public static void main(String[] args) {
+    int a[] = {10, 30, 50, 70, 90};
+    int i, max, min;
+    max = a[0];
+    min = a[0];
+    for (i = 0; i < 5; i++) {
+      if ( (   ) ) > max) {
+        max = a[i];
+      }
+      if ( (   ) ) < min) {
+        min = a[i];
+      }
+    }
+    System.out.print("%d\n", max);
+    System.out.print("%d\n", min);
+  }
+
+//답
+//a[i]
