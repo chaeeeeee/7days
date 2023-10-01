df9da846526ca8f924cd75e7b8686bdb55360d47
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-01 16:01:11 +0900

    틀림

---
+++ 2021/1회/1번.py
@@ -0,0 +1,12 @@
+//다음 Python으로 구현된 프로그램을 분석하여 그 실행 결과를 쓰시오. (단, 출력문의 출력 서식을 준수하시오.)
+class CharClass:
+  a = ['Seoul', 'Kyeongi', 'Incheon', 'Daejeon', 'Daegu', 'Pusan']
+
+myVar = CharClass()
+str01 = ' '
+for i in myVar.a:
+  str01 = str01 + i[0]
+print(str01)
+
+//답
+//SKIDDP
