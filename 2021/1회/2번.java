516c2382af25e053972ce2f534ad6a8f63622614
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-01 16:03:58 +0900

    맞춤

---
+++ 2021/1회/2번.java
@@ -0,0 +1,18 @@
+//다음 Java로 구현된 프로그램을 분석하여 그 실행 결과를 쓰시오. (단, 출력문의 출력 서식을 준수하시오.)
+public class Test {
+  public static void main(String[] args) {
+    int aa[][] = { {45, 50, 75}, {89} };
+    System.out.println(aa[0].length);
+    System.out.println(aa[1].length);
+    System.out.println(aa[0][0]);
+    System.out.println(aa[0][1]);
+    System.out.println(aa[1][0]);
+  }
+}
+
+//답
+//3
+//1
+//45
+//50
+//89
