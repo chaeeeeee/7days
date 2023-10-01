207f9221e4ff2a55cb90628a8cf6f043b0995524
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-01 21:27:44 +0900

    틀림

---
+++ 2023/1회/2번.c
@@ -0,0 +1,20 @@
+//다음 C언어의 출력값을 작성하시오.
+#include <stdio.h> 
+int main(){
+  char a[] = "Art";
+  char* p = NULL;
+  p = a;
+  printf("%s\n", a);
+  printf("%c\n", *p);
+  printf("%c\n", *a);
+  printf("%s\n", p);
+  for(int i = 0; a[i] != '\0'; i++)
+    printf("%c", a[i]);
+}
+
+//답
+//Art
+//A
+//A
+//Art
+//Art
