09e96007b3871c576fa4607b519491fac7180e83
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-01 21:28:48 +0900

    틀림

---
+++ 2023/1회/3번.c
@@ -0,0 +1,15 @@
+//다음 C언어의 출력값을 작성하시오.
+include <stdio.h>
+int main(){
+  char* a = "qwer";
+  char* b = "qwtety";
+  for(int i = 0; a[i] != '\0' ; i++){
+      for(int j = 0; b[j] != '\0'; j++){
+          if(a[i] == b[j]) 
+            printf("%c", a[i]);
+      }
+  }
+}
+
+//답
+//qwe
