1fa0ecdaf4d06831cc43ed0dc1fe6fc7199516e6
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-01 14:48:40 +0900

    맞춤

--- 2020/3회/4번.java
+++ 2020/3회/4번.java
@@ -1,7 +1,7 @@
 //다음 Java로 구현된 프로그램을 분석하여 그 실행 결과를 쓰시오. (단, 출력문의 출력 서식을 준수하시오.)
 public class Test {
   public static void main(String[] args) {
-    int a = 0, num = 0;
+    int a = 0, sum = 0;
     while (a < 10) {
       a++;
       if (a % 2 == 1) {
