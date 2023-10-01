dfca8ae9b6f3bf41621a3fc6ecc19e717c062153
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-01 14:43:09 +0900

    틀림

---
+++ 2020/3회/3번.java
@@ -0,0 +1,29 @@
+//다음 Java 언어로 구현된 프로그램을 분석하여 그 실행 결과를 쓰시오. (단, 출력문의 출력 서식을 준수하시오.)
+abstract class Vehicle {
+  String name;
+  abstract public String getName(String val);
+  public String getName() {
+    return "Vehicle name : " + name;
+  }
+}
+class Car extends Vehicle {
+  private String name;
+  public Car(String val){
+    name = super.name = val;
+  }
+  public String getName(String val){
+    return "Car name : " + val;
+  }
+  public String getName(byte[] val){
+    return "Car name : " + val;
+  }
+}
+public class Test{
+  public static void main(String[] args){
+    Vehicle obj = new Car("Spark");
+    System.out.print(obj.getName());
+  }
+}
+
+//답
+//Vehicle name : Spark
