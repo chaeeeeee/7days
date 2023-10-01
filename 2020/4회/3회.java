b972be4020469666505f0a16959fa697814df8b0
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-01 15:02:06 +0900

    맞춤

---
+++ 2020/4회/3회.java
@@ -0,0 +1,18 @@
+//다음 Java로 구현된 프로그램을 분석하여 괄호 (1), (2)에 들어갈 알맞은 답을 쓰시오.
+public class Test {
+  public static void main(String[] args) {
+    int ary[][] = new int[(  1  )(  2  )];
+    int n = 1;
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; i < 5; j++) {
+        arr[i][j] = j * 3 + i + 1;
+        System.out.print(ary[i][j] + " " );
+      }
+      System.out.println();
+    }
+  }
+}
+
+//답
+//1: 3
+//2: 5
