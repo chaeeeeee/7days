fc4d37969e3acb4e2d8ec124d031457d664b122c
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-01 11:10:59 +0900

    틀림

---
+++ 2019/1회/3번.c
@@ -0,0 +1,18 @@
+//다음 C 언어로 구현한 프로그램을 분석하여 괄호 ( 1 ), ( 2 )에 가장 적합한 답을 쓰시오.
+//예) 1234567을 입력받으면 결과는 1+2+3+4+5+6+7 = 28 출력
+#include <stdio.h>
+main() {
+  int input, sum = 0;
+  scanf("%d", &input);
+  while (1) {
+    if (( 1 ) == 0) // 답안 1
+      break;
+    sum = sum + input % 10;
+    input = input / ( 2 ); // 답안 2
+  }
+  printf("%d\n", sum);
+}
+
+//답
+//1: input
+//2: 10
