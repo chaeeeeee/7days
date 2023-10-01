2e126a7ff256ddf842c4ff1c414fe05dcd5a3b6d
parkchaeeun <114077630+chaeeeeee@users.noreply.github.com>
2023-10-01 14:57:07 +0900

    틀림

---
+++ 2020/4회/2번.java
@@ -0,0 +1,18 @@
+//다음은 변수 n에 저장된 10진수를 2진수로 변환하여 출력하는 Java 프로그램이다. 프로그램을 분석하여 괄호(1), (2)에 들어갈 알맞은 답을 쓰시오.
+public class Test {
+  public static void main(String[] args) {
+    int a[] = new int[8];
+    int i = 0;
+    int n = 10;
+    while ( (  1  ) ) {
+      a[i++] = (  2  );
+      n /= 2;
+    }
+    for (i = 7; i >= 0; i--) {
+      System.out.print(a[i]);
+    }
+  }
+
+//답
+//1: n > 1
+//2: n % 2
